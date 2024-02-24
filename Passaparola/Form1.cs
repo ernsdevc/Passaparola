fbdf9c3536ee37a524cf32cd23045897e3c2c05c
Erensu Deveci <erensudeveci@gmail.com>
2024-02-24 18:16:13 +0300

    Add project files.

---
+++ Passaparola/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Passaparola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int soruNo = 0, dogru = 0, yanlis = 0;
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                switch (soruNo)
+                {
+                    case 1:
+                        if (textBox1.Text == "akdeniz")
+                        {
+                            button1.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button1.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 2:
+                        if (textBox1.Text == "bursa")
+                        {
+                            button2.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button2.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 3:
+                        if (textBox1.Text == "cuma")
+                        {
+                            button3.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button3.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 4:
+                        if (textBox1.Text == "diyarbakır")
+                        {
+                            button4.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button4.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 5:
+                        if (textBox1.Text == "eski")
+                        {
+                            button5.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button5.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 6:
+                        if (textBox1.Text == "ferman")
+                        {
+                            button6.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button6.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 7:
+                        if (textBox1.Text == "güneş")
+                        {
+                            button7.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button7.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                    case 8:
+                        if (textBox1.Text == "cuma")
+                        {
+                            button8.BackColor = Color.Green;
+                            dogru++;
+                            label2.Text = dogru.ToString();
+                        }
+                        else
+                        {
+                            button8.BackColor = Color.Red;
+                            yanlis++;
+                            label3.Text = yanlis.ToString();
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.Text = "Sonraki";
+            soruNo++;
+            this.Text = soruNo.ToString();
+
+            if (soruNo == 1)
+            {
+                richTextBox1.Text = "Ülkemizin güney kısmındaki kıyı bölgesi?";
+                button1.BackColor = Color.Yellow;
+            }
+            if (soruNo == 2)
+            {
+                richTextBox1.Text = "Yeşilliğiyle ünlü Marmara ilimiz?";
+                button2.BackColor = Color.Yellow;
+            }
+            if (soruNo == 3)
+            {
+                richTextBox1.Text = "Müslümanların kutsal günü?";
+                button3.BackColor = Color.Yellow;
+            }
+            if (soruNo == 4)
+            {
+                richTextBox1.Text = "Karpuzuyla ünlü ilimiz?";
+                button4.BackColor = Color.Yellow;
+            }
+            if (soruNo == 5)
+            {
+                richTextBox1.Text = "Yeni kelimesinin zıt anlamlısı?";
+                button5.BackColor = Color.Yellow;
+            }
+            if (soruNo == 6)
+            {
+                richTextBox1.Text = "Padişahın emirlerinin yazılı hali?";
+                button6.BackColor = Color.Yellow;
+            }
+            if (soruNo == 7)
+            {
+                richTextBox1.Text = "Dünyanın ısı kaynağı?";
+                button7.BackColor = Color.Yellow;
+            }
+            if (soruNo == 8)
+            {
+                richTextBox1.Text = "Öğrencilerin kötü karne getirince kaçtığı nesne?";
+                button8.BackColor = Color.Yellow;
+            }
+            if (soruNo == 9)
+            {
+                richTextBox1.Text = "Gülüyle ünlü ilimiz?";
+                button9.BackColor = Color.Yellow;
+            }
+            if (soruNo == 10)
+            {
+                richTextBox1.Text = "Mersinin diğer ismi?";
+                button10.BackColor = Color.Yellow;
+            }
+            if (soruNo == 11)
+            {
+                richTextBox1.Text = "Askeri bir topluluk?";
+                button11.BackColor = Color.Yellow;
+            }
+            if (soruNo == 12)
+            {
+                richTextBox1.Text = "Malatyanın meşhur meyvesi?";
+                button12.BackColor = Color.Yellow;
+            }
+            if (soruNo == 13)
+            {
+                richTextBox1.Text = "Her yıl bahar aylarında düzenlenen meşhur çiçek festivali?";
+                button13.BackColor = Color.Yellow;
+            }
+            if (soruNo == 14)
+            {
+                richTextBox1.Text = "Yılın üçüncü ayı?";
+                button14.BackColor = Color.Yellow;
+            }
+            if (soruNo == 15)
+            {
+                richTextBox1.Text = "Üflemeli bir müzik aleti?";
+                button15.BackColor = Color.Yellow;
+            }
+            if (soruNo == 16)
+            {
+                richTextBox1.Text = "Halk şairi?";
+                button16.BackColor = Color.Yellow;
+            }
+            if (soruNo == 17)
+            {
+                richTextBox1.Text = "Çocukların çok sevmediği pirinç ve havuç gibi sebzelerle yapılan yemek?";
+                button17.BackColor = Color.Yellow;
+            }
+            if (soruNo == 18)
+            {
+                richTextBox1.Text = "11 Ayın Sultanı?";
+                button18.BackColor = Color.Yellow;
+            }
+            if (soruNo == 19)
+            {
+                richTextBox1.Text = "İngilizcede yılan?";
+                button19.BackColor = Color.Yellow;
+            }
+            if (soruNo == 20)
+            {
+                richTextBox1.Text = "Türkiyenin megatrarı?";
+                button20.BackColor = Color.Yellow;
+            }
+            if (soruNo == 21)
+            {
+                richTextBox1.Text = "Ümit kelimesinin eş anlamlısı?";
+                button21.BackColor = Color.Yellow;
+            }
+            if (soruNo == 22)
+            {
+                richTextBox1.Text = "Kahvaltısı ile ünlü ilimiz?";
+                button22.BackColor = Color.Yellow;
+            }
+            if (soruNo == 23)
+            {
+                richTextBox1.Text = "Şimşek kelimesinin eş anlamlısı?";
+                button23.BackColor = Color.Yellow;
+            }
+            if (soruNo == 24)
+            {
+                richTextBox1.Text = "Ege bölgesinde en çok ağacı bulunan yağı da yapılan bir kahvaltı besini?";
+                button24.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
